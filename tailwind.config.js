985f53913d00c2d38945075179c9616bb668d918
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-12-15 14:14:48 +0100

    add responsive header with glow background and clean layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 export default {
-  content: ["./index.html", "./projects/**/*.html", "./assets/js/**/*.js"],
+  content: ["./*.html", "./projects/**/*.html", "./assets/js/**/*.js"],
   darkMode: "class",
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+        brand: ["Space Grotesk", "system-ui", "sans-serif"],
+      },
+    },
+  },
   plugins: [],
 };
